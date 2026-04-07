8be9d679bbf87d16d8ba506eb63438ab0439482f
RAMOHAM <reda.ali.moham@gmail.com>
2026-04-07 17:48:23 +0100

    initial

---
+++ src/main/java/org/example/fixoraserver/FixoraServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.fixoraserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixoraServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixoraServerApplication.class, args);
+    }
+
+}
