54651c6699bc12456acfb2cde6dc560255aa41bb
RAMOHAM <reda.ali.moham@gmail.com>
2026-04-12 20:21:01 +0100

    small annotation fixes

--- src/main/java/org/example/fixoraserver/booking/controller/BookingController.java
+++ src/main/java/org/example/fixoraserver/booking/controller/BookingController.java
@@ -1,11 +1,9 @@
 package org.example.fixoraserver.booking.controller;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.example.fixoraserver.booking.model.Booking;
 import org.example.fixoraserver.booking.service.BookingService;
-import org.example.fixoraserver.email.BookingEmailService;
-import org.example.fixoraserver.email.EmailService;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +18,7 @@ public class BookingController {
     private final BookingService bookingService;
 
     @PostMapping
-    public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+    public ResponseEntity<@NonNull Booking> createBooking(@RequestBody Booking booking) {
         Booking newBooking = bookingService.createBooking(booking);
         return ResponseEntity.status(HttpStatus.CREATED).body(newBooking);
     }
--- src/main/java/org/example/fixoraserver/booking/repository/BookingRepository.java
+++ src/main/java/org/example/fixoraserver/booking/repository/BookingRepository.java
@@ -1,7 +1,8 @@
 package org.example.fixoraserver.booking.repository;
 
+import lombok.NonNull;
 import org.example.fixoraserver.booking.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends JpaRepository<@NonNull Booking,@NonNull Long> {
 }
